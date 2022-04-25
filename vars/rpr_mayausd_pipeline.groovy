9ca7f41e1d58ae1a5590e3fd5c9b9eecc7ce8632
Speedwag00n <INemankov@luxoft.com>
2022-04-25 19:10:25 +0300

    Update rename command (Maya USD)

--- vars/rpr_mayausd_pipeline.groovy
+++ vars/rpr_mayausd_pipeline.groovy
@@ -384,7 +384,7 @@ def executeBuildWindows(Map options) {
 
                 if (options.branch_postfix) {
                     bat """
-                        rename RPRMayaUSD_Setup.exe RPRMayaUSD_Setup_${options.pluginVersion}_(${options.branch_postfix}).exe
+                        rename RPRMayaUSD_Setup* RPRMayaUSD_Setup_${options.pluginVersion}_(${options.branch_postfix}).exe
                     """
                 }
 
