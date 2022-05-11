2aee917c99aa522371b7db3a8eaf4c5ca27d9dc9
Speedwag00n <INemankov@luxoft.com>
2022-05-11 20:02:31 +0300

    Increase Anari cache building timeout (10mins -> 20mins)

--- vars/rpr_anari_pipeline.groovy
+++ vars/rpr_anari_pipeline.groovy
@@ -131,7 +131,7 @@ def executeTests(String osName, String asicName, Map options) {
         outputEnvironmentInfo(osName, "", options.currentTry)
 
         withNotifications(title: options["stageName"], options: options, configuration: NotificationConfiguration.BUILD_CACHE) {
-            timeout(time: "10", unit: "MINUTES") {
+            timeout(time: "20", unit: "MINUTES") {
                 buildRenderCache(osName, options.stageName, options.currentTry)
             }
         }
