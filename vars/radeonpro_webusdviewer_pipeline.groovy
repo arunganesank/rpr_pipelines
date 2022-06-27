865124197da5fd70dd69324f26d9ef0e022e3423
Speedwag00n <INemankov@luxoft.com>
2022-06-27 18:07:25 +0300

    Remove PreBuild and Deploy stages (Web USD Viewer)

--- vars/radeonpro_webusdviewer_pipeline.groovy
+++ vars/radeonpro_webusdviewer_pipeline.groovy
@@ -2,8 +2,7 @@ import groovy.transform.Field
 
 @Field final String PROJECT_REPO = "git@github.com:Radeon-Pro/WebUsdViewer.git"
 
-def executeBuildWindows(Map options)
-{
+def executeBuildWindows(Map options) {
     Boolean failure = false
     String webrtcPath = "C:\\JN\\thirdparty\\webrtc"
 
@@ -34,11 +33,9 @@ def executeBuildWindows(Map options)
         println("Error during build on Windows")
         println(e.toString())
         failure = true
-    }
-    finally {
+    } finally {
         archiveArtifacts "*.log"
     }
-    
 
     if (failure) {
         currentBuild.result = "FAILED"
@@ -47,21 +44,23 @@ def executeBuildWindows(Map options)
 }
 
 
-def executeBuildLinux(Map options)
-{
+def executeBuildLinux(Map options) {
     Boolean failure = false
     println "[INFO] Start build" 
     downloadFiles("/volume1/CIS/radeon-pro/webrtc-linux/", "${CIS_TOOLS}/../thirdparty/webrtc", "--quiet")
     try {
-        println "[INFO] Start build" 
+        println "[INFO] Start build"
+
         sh """
             mkdir --parents Build/Install
         """
+
         if (!options.rebuildDeps){
             downloadFiles("/volume1/CIS/WebUSD/${options.osName}/USD", "./Build/Install", "--quiet")
             // Because modes resetting after downloading from NAS
             sh """ chmod -R 775 ./Build/Install/USD"""
         }
+
         sh """
             cmake --version >> ${STAGE_NAME}.log 2>&1
             python3 --version >> ${STAGE_NAME}.log 2>&1
@@ -71,19 +70,22 @@ def executeBuildLinux(Map options)
             export OS=
             python3 Tools/Build.py -v >> ${STAGE_NAME}.log 2>&1
         """
+
         if (options.updateDeps){
             uploadFiles("./Build/Install/USD", "/volume1/CIS/WebUSD/${options.osName}", "--delete")
         }
+
         println("[INFO] Start building & sending docker containers to repo")
         withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'WebUsdDockerRegisterHost', usernameVariable: 'remoteHost', passwordVariable: 'remotePort']]){
             String deployArgs = "-ba -da"
-            containersBaseName = "docker.${remoteHost}/" 
+            containersBaseName = "docker.${remoteHost}/"
+
             if (env.CHANGE_URL){
                 deployArgs = "-ba"
                 containersBaseName = ""
                 remoteHost = ""
-                options["deployEnvironment"] = "pr"
             }
+
             env["WEBUSD_BUILD_REMOTE_HOST"] = remoteHost
             env["WEBUSD_BUILD_LIVE_CONTAINER_NAME"] = containersBaseName + "live"
             env["WEBUSD_BUILD_ROUTE_CONTAINER_NAME"] = containersBaseName + "route"
@@ -93,8 +95,10 @@ def executeBuildLinux(Map options)
 
             sh """python3 Tools/Docker.py $deployArgs -v -c $options.deployEnvironment"""
         }
+
         println("[INFO] Finish building & sending docker containers to repo")
         sh "rm WebUsdWebServer/.env.production"
+
         if (options.generateArtifact){
             sh """
                 tar -C Build/Install -czvf "WebUsdViewer_Ubuntu20.tar.gz" .
@@ -113,17 +117,116 @@ def executeBuildLinux(Map options)
         currentBuild.result = "FAILED"
         error "error during build"
     }
-    currentBuild.result = "SUCCESS"
+
+    if (deploy) {
+        println "[INFO] Start deploying on $options.deployEnvironment environment"
+        failure = false
+        Boolean status = true
+
+        try {
+            if (env.CHANGE_URL) {
+                println "[INFO] Local deploying for sanytize checks"
+                String composePath = "/usr/local/bin/docker-compose"
+
+                downloadFiles("/volume1/CIS/WebUSD/Additional/pr.yml", ".", "--quiet")
+                sh """$composePath -f pr.yml up -d"""
+
+                List containersNamesAssociations = [
+                    "${env.WEBUSD_BUILD_LIVE_CONTAINER_NAME}",
+                    "${env.WEBUSD_BUILD_ROUTE_CONTAINER_NAME}",
+                    "${env.WEBUSD_BUILD_STORAGE_CONTAINER_NAME}",
+                    "${env.WEBUSD_BUILD_STREAM_CONTAINER_NAME}",
+                    "front"
+                ]
+
+                for (i=0; i < 5; i++) {
+                    try {
+                        sleep(5)
+                        for (name in containersNamesAssociations) {
+                            result = sh (
+                                script: "$composePath -f pr.yml ps --services --filter \"status=running\" | grep $name",
+                                returnStdout: true,
+                                returnStatus: true
+                            )
+                            println result
+                            if (result == 1) {
+                                throw new Exception("""[ERROR] Sanytize checks failed. Service ${name} doesn't work""")
+                            }
+                        }
+                        sh """$composePath -f pr.yml stop"""
+                        println "[INFO] Sanytize checks successfully passed"
+                        break
+                    } catch (e) {
+                        println "[ERROR] Sanytize checks failed. Trying again"
+                    }
+                }
+
+                if (!status) {
+                    sh """$composePath -f pr.yml stop"""
+                    throw new Exception("[ERROR] Sanytize checks failed. All attempts have been exhausted.")
+                }
+            } else {
+                println "[INFO] Send deploy command"
+                withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'WebUsdDockerRegisterHost', usernameVariable: 'remoteHost', passwordVariable: 'remotePort']]){
+                    for (i=0; i < 5; i++) {
+                        res = sh(
+                            script: "curl --insecure https://admin.${remoteHost}/deploy?configuration=${options.deployEnvironment}",
+                            returnStdout: true,
+                            returnStatus: true
+                        )
+
+                        println ("RES - ${res}")
+
+                        if (res == 0) {
+                            println "[INFO] Successfully sended"
+                            break
+                        } else {
+                            println "[ERROR] Host not available. Try again"
+                        }
+                    }
+
+                    if (!status) {
+                        throw new Exception("[ERROR] Host not available. Retries exceeded")
+                    }
+                }
+            }
+        } catch (e) {
+            println "[ERROR] Error during deploy"
+            println(e.toString())
+            failure = true
+            status = false
+        }
+
+        if (failure) {
+            currentBuild.result = "FAILED"
+            error "error during deploy"
+        }
+
+        notifyByTg(options)
+    }
 }
 
-def executeBuild(String osName, Map options)
-{   
+
+def executeBuild(String osName, Map options) {  
     try {
         cleanWS(osName)
         checkoutScm(branchName: options.projectBranch, repositoryUrl: options.projectRepo)
+
+        options.commitAuthor = bat (script: "git show -s --format=%%an HEAD ",returnStdout: true).split('\r\n')[2].trim()
+        commitMessage = bat (script: "git log --format=%%B -n 1", returnStdout: true)
+        options.commitSHA = bat (script: "git log --format=%%H -1 ", returnStdout: true).split('\r\n')[2].trim()
+        println "The last commit was written by ${options.commitAuthor}."
+        println "Commit message: ${commitMessage}"
+        println "Commit SHA: ${options.commitSHA}"
+
+        if (env.CHANGE_URL) {
+            // TODO add github notification
+        }
+
         outputEnvironmentInfo(osName)
         downloadFiles("/volume1/CIS/WebUSD/Additional/envs/webusd.env.${options.deployEnvironment}", "./WebUsdWebServer", "--quiet")
         sh "mv ./WebUsdWebServer/webusd.env.${options.deployEnvironment} ./WebUsdWebServer/.env.production"
+
         switch(osName) {
             case 'Windows':
                 executeBuildWindows(options)
@@ -140,119 +243,18 @@ def executeBuild(String osName, Map options)
     } finally {
         archiveArtifacts "*.log"
     }
-    currentBuild.result = "SUCCESS"
-}
-
-def executePreBuild(Map options)
-{
-    
-    dir('WebUsdViewer') {
-        checkoutScm(branchName: options.projectBranch, repositoryUrl: options.projectRepo, disableSubmodules: true)
-        options.commitAuthor = bat (script: "git show -s --format=%%an HEAD ",returnStdout: true).split('\r\n')[2].trim()
-        commitMessage = bat (script: "git log --format=%%B -n 1", returnStdout: true)
-        options.commitSHA = bat (script: "git log --format=%%H -1 ", returnStdout: true).split('\r\n')[2].trim()
-        println "The last commit was written by ${options.commitAuthor}."
-        println "Commit message: ${commitMessage}"
-        println "Commit SHA: ${options.commitSHA}"
-
-        if (env.CHANGE_URL){
-            // TODO add github notification
-        }
-    }
-}
-
-def executeTest(Map options){
-    println "[WARN] Test for this pipeline doesn't exists. Skip Tests stage."
-}
-
-def executeDeploy(Map options, List platformList, List testResultList)
-{
-    println "[INFO] Start deploying on $options.deployEnvironment environment"
-    failure = false
-    Boolean status = true
-    try{
-        if (env.CHANGE_URL){
-            println "[INFO] Local deploying for sanytize checks"
-            String composePath = "/usr/local/bin/docker-compose"
-
-            downloadFiles("/volume1/CIS/WebUSD/Additional/pr.yml", ".", "--quiet")
-            sh """$composePath -f pr.yml up -d"""
-            List containersNamesAssociations = [
-                "${env.WEBUSD_BUILD_LIVE_CONTAINER_NAME}",
-                "${env.WEBUSD_BUILD_ROUTE_CONTAINER_NAME}",
-                "${env.WEBUSD_BUILD_STORAGE_CONTAINER_NAME}",
-                "${env.WEBUSD_BUILD_STREAM_CONTAINER_NAME}",
-                "front"
-            ]
-            for (i=0; i < 5; i++){
-                try{
-                    sleep(5)
-                    for (name in containersNamesAssociations){
-                        result = sh (
-                            script: "$composePath -f pr.yml ps --services --filter \"status=running\" | grep $name",
-                            returnStdout: true,
-                            returnStatus: true
-                        )
-                        println result
-                        if (result == 1){
-                            throw new Exception("""[ERROR] Sanytize checks failed. Service ${name} doesn't work""")
-                        }
-                    }
-                    sh """$composePath -f pr.yml stop"""
-                    println "[INFO] Sanytize checks successfully passed"
-                    break
-                }catch (e){
-                    println "[ERROR] Sanytize checks failed. Trying again"
-                }
-            }
-            if (!status){
-                sh """$composePath -f pr.yml stop"""
-                throw new Exception("[ERROR] Sanytize checks failed. All attempts have been exhausted.")
-            }
-        }else {
-            println "[INFO] Send deploy command"
-            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'WebUsdDockerRegisterHost', usernameVariable: 'remoteHost', passwordVariable: 'remotePort']]){
-                for (i=0; i < 5; i++){
-                    res = sh(
-                        script: "curl --insecure https://admin.${remoteHost}/deploy?configuration=${options.deployEnvironment}",
-                        returnStdout: true,
-                        returnStatus: true
-                    )
-                    println ("RES - ${res}")
-                    if (res == 0){
-                        println "[INFO] Successfully sended"
-                        break
-                    }else{
-                        println "[ERROR] Host not available. Try again"
-                    }
-                }
-                if (!status){
-                    throw new Exception("[ERROR] Host not available. Retries exceeded")
-                }
-            }
-        }
-    }catch (e){
-        println "[ERROR] Error during deploy"
-        println(e.toString())
-        failure = true
-        status = false
-    }
-    if (failure){
-        currentBuild.result = "FAILED"
-        error "error during deploy"
-    }
-    notifyByTg(options)
-    
 }
 
 
-def notifyByTg(Map options){
+def notifyByTg(Map options) {
     String status_message = currentBuild.result.contains("FAILED") ? "Success" : "Failed"
     Boolean is_pr = env.CHANGE_URL != null
     String branchName = env.CHANGE_URL ?: options.projectBranch
+
     if (branchName.contains("origin")){
         branchName = branchName.split("/", 2)[1]
     }
+
     String branchURL = is_pr ? env.CHANGE_URL : "https://github.com/Radeon-Pro/WebUsdViewer/tree/${branchName}" 
     withCredentials([string(credentialsId: "WebUsdTGBotHost", variable: "tgBotHost")]){
         res = sh(
@@ -264,10 +266,9 @@ def notifyByTg(Map options){
 }
 
 
-
 def call(
     String projectBranch = "",
-    String platforms = 'Windows;Ubuntu20',
+    String platforms = 'Ubuntu20',
     Boolean enableNotifications = true,
     Boolean generateArtifact = false,
     Boolean deploy = true,
@@ -275,8 +276,8 @@ def call(
     Boolean rebuildDeps = false,
     Boolean updateDeps = false
 ) {
-    if (env.BRANCH_NAME){
-        switch (env.BRANCH_NAME){
+    if (env.BRANCH_NAME) {
+        switch (env.BRANCH_NAME) {
             case "develop":
                 deployEnvironment = "dev"
                 break
@@ -287,8 +288,11 @@ def call(
                 deployEnvironment = "test3"
                 break
         }
+    } else if (env.CHANGE_URL) {
+        deployEnvironment = "pr"
     }
-    multiplatform_pipeline(platforms, this.&executePreBuild, this.&executeBuild, this.&executeTests, this.&executeDeploy,
+
+    multiplatform_pipeline(platforms, null, this.&executeBuild, null, null,
                             [projectBranch:projectBranch,
                             projectRepo:PROJECT_REPO,
                             rebuildDeps:rebuildDeps,
