e97247ae2659073c0de3b515761b58aaf8c11786
Flame0n <lexainb3@gmail.comm>
2022-07-07 00:46:36 +0200

    Created script for uploading engine to svn

--- vars/rpr_hybrid_ue_pipeline.groovy
+++ vars/rpr_hybrid_ue_pipeline.groovy
@@ -164,17 +164,22 @@ def executeBuildWindows(String projectName, Map options) {
                 makeArchiveArtifacts(name: ARTIFACT_NAME, storeOnNAS: options.storeOnNAS)
             }
 
+            
             if (options.saveEngine) {
                 dir("RPRHybrid-UE") {
-                    String ARTIFACT_NAME = "${projectName}_editor.zip"
-                    bat(script: '%CIS_TOOLS%\\7-Zip\\7z.exe a' + " \"${ARTIFACT_NAME}\" . -xr!*.obj -xr!*.pdb -xr!*.vs -xr!*.git -xr!*@tmp*")
-                    makeArchiveArtifacts(name: ARTIFACT_NAME, storeOnNAS: options.storeOnNAS)
-                    utils.removeFile(this, "Windows", ARTIFACT_NAME)
 
+                    bat """
+                        svn co svn://cis.nas.stvcis.com/ToyShopEditor .
+                        svn resolve --accept working -R .
+                        svn propset svn:global-ignores -F .svn_ignore .
+                        svn add * --force --quiet
+                        svn commit -m ""
+                    """
+                    /*
                     ARTIFACT_NAME = "${projectName}_debug.zip"
                     bat(script: '%CIS_TOOLS%\\7-Zip\\7z.exe a' + " \"${ARTIFACT_NAME}\" -ir!*.pdb -xr!*@tmp*")
                     makeArchiveArtifacts(name: ARTIFACT_NAME, storeOnNAS: options.storeOnNAS)
-                    utils.removeFile(this, "Windows", ARTIFACT_NAME)
+                    utils.removeFile(this, "Windows", ARTIFACT_NAME) */
                 }
             }
         }
