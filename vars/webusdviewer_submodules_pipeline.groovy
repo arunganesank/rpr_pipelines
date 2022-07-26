9ee1abdef1b8775290f56910deb01301934592b7
Flame0n <lexainb3@gmail.comm>
2022-07-26 19:00:04 +0200

    Added debug code

--- vars/webusdviewer_submodules_pipeline.groovy
+++ vars/webusdviewer_submodules_pipeline.groovy
@@ -1,3 +1,23 @@
-def call(){
+@Field final String PROJECT_REPO = "git@github.com:s1lentssh/WebUsdLiveServer.git"
+
+def call() {
     println("He-he")
+
+    stage("Increment version") {
+        node("GitPublisher") {
+            WS("Increment version"){
+                checkoutScm(branchName: env.BRANCH_NAME, repositoryUrl: PROJECT_REPO)
+
+                String version = bat(script: "FOR /F %i IN (VERSION.txt) DO @echo %i", returnStdout: true).trim()
+                println("Get version: ", version)
+
+                switch(env.BRANCH_NAME) {
+                    case "master":
+                        break
+                    case "develop":
+                        break
+                }
+            }
+        }
+    }
 }
