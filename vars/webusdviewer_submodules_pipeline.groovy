23dd925b394c12fae8da4fe4a6572699c4c329fe
Flame0n <lexainb3@gmail.comm>
2022-07-26 20:14:50 +0200

    WIP

--- vars/webusdviewer_submodules_pipeline.groovy
+++ vars/webusdviewer_submodules_pipeline.groovy
@@ -1,22 +1,41 @@
 
 def call() {
     String PROJECT_REPO = "git@github.com:s1lentssh/WebUsdLiveServer.git"
-    println("He-he")
 
     stage("Increment version") {
         node("GitPublisher") {
-            WS("Increment version"){
+            ws("WS/WebUsdLiveServer_increment") {
                 checkoutScm(branchName: env.BRANCH_NAME, repositoryUrl: PROJECT_REPO)
 
-                String version = bat(script: "FOR /F %i IN (VERSION.txt) DO @echo %i", returnStdout: true).trim()
-                println("Get version: ", version)
+                String version = bat(script: "FOR /F %%i IN (VERSION.txt) DO @echo %%i", returnStdout: true).trim() as String
 
-                switch(env.BRANCH_NAME) {
-                    case "master":
-                        break
-                    case "develop":
-                        break
+                println("Current version of: " + version)
+
+                def splitted = version.split(".")
+                
+                if (splitted.size(3)) {
+                    def major = splitted[0]
+                    def firstMinor = splitted[1]
+                    def lastMinor = splitted[2]
+                    
+                    switch(env.BRANCH_NAME) {
+                        case "master":
+                            firstMinor++
+                            break
+                        case "develop":
+                            lastMinor++
+                            break
+                    }
+
+                    bat """
+                        break > VERSION.txt
+                        echo "${major}.${firstMinor}.${lastMinor}" > VERSION.txt
+                    """
                 }
+
+                version = bat(script: "FOR /F %%i IN (VERSION.txt) DO @echo %%i", returnStdout: true).trim() as String
+
+                println("Newest version of: " + version)
             }
         }
     }
