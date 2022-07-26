fd8c4f942f7b57b9376875bae8644b4534b1ceaf
Wieceslaw <slava.astra@yahoo.com>
2022-07-26 17:28:29 +0200

    Install and uninstall driver from pipeline

--- vars/amdDriverUpdate.groovy
+++ vars/amdDriverUpdate.groovy
@@ -60,31 +60,59 @@ def planUpdate(osName, gpuNames, options, updateTasks) {
                             DRIVER_PAGE_URL = "https://www.amd.com/en/support/graphics/amd-radeon-6000-series/amd-radeon-6800-series/amd-radeon-rx-6800-xt"
                             
                             cleanWS()
-                            def status
+                            def status, driver_path
 
                             switch(osName) {
                                 case "Windows":
+                                    driver_path = "C:\\AMD\\driver\\"
                                     bat "${CIS_TOOLS}\\driver_detection\\amd_request.bat \"${DRIVER_PAGE_URL}\" ${env.WORKSPACE}\\page.html >> page_download_${it}.log 2>&1 "
 
                                     withEnv(["PATH=c:\\python39\\;c:\\python39\\scripts\\;${PATH}"]) {
                                         python3("-m pip install -r ${CIS_TOOLS}\\driver_detection\\requirements.txt >> parse_stage_${it}.log 2>&1")
-                                        status = bat(returnStatus: true, script: "python ${CIS_TOOLS}\\driver_detection\\parse_driver.py --os win --html_path ${env.WORKSPACE}\\page.html --installer_dst ${env.WORKSPACE}\\driver.exe --drivers_dir C:\\AMD >> parse_stage_${it}.log 2>&1")
+                                        status = bat(returnStatus: true, script: "python ${CIS_TOOLS}\\driver_detection\\parse_driver.py --os win --html_path ${env.WORKSPACE}\\page.html --installer_dst ${env.WORKSPACE}\\driver.exe --win_driver_path ${driver_path} >> parse_stage_${it}.log 2>&1")
+                                        if (status == 0) {
+                                            println("[INFO] Newer driver was found. Trying to install...")
+                                            bat "${driver_path}\\Setup.exe -INSTALL >> installation_${it}.log 2>&1"
+                                        }
                                     }
                                     break
                                 case "Ubuntu20":
+                                    driver_path = "${env.WORKSPACE}/amdgpu-install.deb"
                                     sh "${CIS_TOOLS}/driver_detection/amd_request.sh \"${DRIVER_PAGE_URL}\" ${env.WORKSPACE}/page.html >> page_download_${it}.log 2>&1 "
-                                    status = sh(returnStatus: true, script: "sudo ${CIS_TOOLS}/driver_detection/perform_driver_detection.sh ubuntu20 ${env.WORKSPACE}/page.html ${env.WORKSPACE}/amdgpu-install.deb >> parse_stage_${it}.log 2>&1")
+
+                                    status = sh(returnStatus: true, script: "sudo ${CIS_TOOLS}/driver_detection/perform_driver_detection.sh ubuntu20 ${env.WORKSPACE}/page.html ${driver_path} >> parse_stage_${it}.log 2>&1")
+                                    if (status == 0) {
+                                        println("[INFO] Newer was found. Uninstalling previous driver...")
+                                        sh "sudo amdgpu-install -y --uninstall >> uninstallation_${it}.log 2>&1"
+                                        println("[INFO] Driver uninstalled. Reboot ${it}...")
+                                        utils.reboot(this, "Unix")
+                                        sh "sudo apt-get purge -y amdgpu-install >> uninstallation_${it}.log 2>&1"
+
+                                        println("[INFO] Trying to install new driver...")
+                                        sh """
+                                            sudo apt install -y ${driver_path} >> installation_${it}.log 2>&1 && \
+                                            sudo amdgpu-install --usecase=workstation -y --vulkan=pro --opencl=rocr,legacy --accept-eula >> installation_${it}.log 2>&1 \
+                                        """
+                                    }
                                     break
                                 default:
                                     println "[WARNING] ${osName} is not supported"
                             }
 
-                            if (status == 404) {
-                                println("[INFO] Newer driver not found")
-                            } else {
-                                println("[INFO] Newer driver was installed")
-                                newerDriverInstalled = true
-                                utils.reboot(this, isUnix() ? "Unix" : "Windows")
+                            switch(status) {
+                                case 0:
+                                    println("[INFO] Newer driver was installed")
+                                    newerDriverInstalled = true
+                                    utils.reboot(this, isUnix() ? "Unix" : "Windows")
+                                    break
+                                case 1:
+                                    throw new Exception("Error during parsing stage")
+                                    break
+                                case 404:
+                                    println("[INFO] Newer driver not found")
+                                    break
+                                default:
+                                    throw new Exception("Unknown exit code")
                             }
                         } catch(e) {
                             println(e.toString());
