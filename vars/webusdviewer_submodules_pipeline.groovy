43f4dda17dec63835ebf417ac53594f9b71023b9
Flame0n <lexainb3@gmail.comm>
2022-07-27 20:02:18 +0200

    Added requested changes

--- vars/webusdviewer_submodules_pipeline.groovy
+++ vars/webusdviewer_submodules_pipeline.groovy
@@ -6,43 +6,34 @@ def call(String projectName, String projectRepo) {
             ws("WS/${projectName}_increment") {
                 checkoutScm(branchName: env.BRANCH_NAME, repositoryUrl: projectRepo)
 
-                def script = """
-                    @echo off
-                    FOR /F %%i IN (VERSION.txt) DO @echo %%i
-                """
-                
-                String version = bat(script: script, returnStdout: true).trim() as String
+                String version = context.readFile("VERSION.txt") 
 
                 println("Current version of submodule: " + version)
                 def splitted = version.split("\\.")
 
                 if (splitted.size() == 3 && env.BRANCH_NAME == "test") { // debug delete it: "&& env.BRANCH_NAME == "test""
-                    int major = splitted[0] as Integer
-                    int firstMinor = splitted[1] as Integer
-                    int lastMinor = splitted[2] as Integer
-                    
                     switch(env.BRANCH_NAME) {
                         /*
                         case "master":
-                            firstMinor += 1
+                            def new_version = version_inc(version, 2, '.')
                             break
                         case "develop":
-                            lastMinor += 1
+                            def new_version = version_inc(version, 3, '.')
                             break
                         */
                         case "test":   // test branch
-                            firstMinor += 1
+                            def new_version = version_inc(version, 3, '.')
                             break
                     }
 
                     bat """
                             break > VERSION.txt
-                            echo ${major}.${firstMinor}.${lastMinor} > VERSION.txt 
+                            echo ${new_version} > VERSION.txt 
                             git commit VERSION.txt -m "buildmaster: version update to ${major}.${firstMinor}.${lastMinor}"
                             git push origin HEAD:${env.BRANCH_NAME}
                         """
 
-                    version = bat(script: script, returnStdout: true).trim() as String
+                    version = context.readFile("VERSION.txt")
                     println("Newest version of submodule: " + version)
                 } else {
                     throw new Exception("Wrong version formatting")
