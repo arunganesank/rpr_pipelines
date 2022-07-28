51c463ee9c123ae4e7ef57b5413300961dccfe27
Flame0n <lexainb3@gmail.comm>
2022-07-28 23:18:53 +0200

    Fixed syntax error

--- vars/radeonpro_webusdviewer_pipeline.groovy
+++ vars/radeonpro_webusdviewer_pipeline.groovy
@@ -415,8 +415,8 @@ def executePreBuild(Map options) {
         githubNotificator.init(options)
         options["githubNotificator"] = githubNotificator
     }
-    options.platforms.split(';').each() { platform ->
-        List tokens = platform.tokenize(':')
+    options.platforms.split(';').each() { os ->
+        List tokens = os.tokenize(':')
         String platform = tokens.get(0) 
         platform = platform == "Windows" ?: "Web application"
         GithubNotificator.createStatus("Build", platform, "queued", options, "Scheduled", "${env.JOB_URL}")
