514fd4d5bbb105a0fbd4a8fa8c0415ac4d63c7dd
Speedwag00n <INemankov@luxoft.com>
2022-08-29 10:45:51 +0200

    Stabilize autotests execution

--- vars/radeonpro_webusdviewer_autotests_pipeline.groovy
+++ vars/radeonpro_webusdviewer_autotests_pipeline.groovy
@@ -11,37 +11,46 @@ def call(String testsBranch, String tests, String instance) {
                     ws("WS/WebUsdViewer_Autotests") {
                         cleanWS("Windows")
 
-                        checkoutScm(branchName: testsBranch, repositoryUrl: AUTOTESTS_REPO)
+                        dir("jobs_test_web_viewer") {
+                            try {
+                                checkoutScm(branchName: testsBranch, repositoryUrl: AUTOTESTS_REPO)
 
-                        String configPrefix = "testing"
+                                String configPrefix = "testing"
 
-                        if (instance != "prod") {
-                            dir("Utils/ConfigurationFiles") {
-                                String content = readFile("${configPrefix}Config.yaml")
-                                content = content.replace("webusd", "${instance}.webusd")
-                                writeFile(file: "${configPrefix}Config.yaml", text: content)
+                                if (instance != "prod") {
+                                    dir("Utils/ConfigurationFiles") {
+                                        String content = readFile("${configPrefix}Config.yaml")
+                                        content = content.replace("webusd", "${instance}.webusd")
+                                        writeFile(file: "${configPrefix}Config.yaml", text: content)
+                                    }
+                                }
+
+                                dir("scripts") {
+                                    withEnv(["PATH=c:\\python39\\;${PATH}"]) {
+                                        bat """
+                                            run.bat ${tests} ${configPrefix} >> ..\\autotests.log 2>&1
+                                        """
+
+                                        bat """
+                                            save_report.bat >> ..\\build_report.log 2>&1
+                                        """
+                                    }
+                                }
+
+                                publishHTML([allowMissing: false,
+                                    alwaysLinkToLastBuild: false,
+                                    keepAll: true,
+                                    reportDir: "Report",
+                                    reportFiles: "index.html",
+                                    reportName: "Test Report"]
+                                )
+                            } catch (e) {
+                                println("[ERROR] Failed to execute autotests")
+                                throw e
+                            } finally {
+                                archiveArtifacts artifacts: "*.log", allowEmptyArchive: true
                             }
                         }
-
-                        dir("scripts") {
-                            withEnv(["PATH=c:\\python37\\;${PATH}"]) {
-                                bat """
-                                    run.bat ${tests} ${configPrefix} >> ..\\autotests.log 2>&1
-                                """
-
-                                bat """
-                                    save_report.bat >> ..\\build_report.log 2>&1
-                                """
-                            }
-                        }
-
-                        publishHTML([allowMissing: false,
-                            alwaysLinkToLastBuild: false,
-                            keepAll: true,
-                            reportDir: "Report",
-                            reportFiles: "index.html",
-                            reportName: "Test Report"]
-                        )
                     }
                 }
             }
