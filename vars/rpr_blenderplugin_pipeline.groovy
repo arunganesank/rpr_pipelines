0bd4dcbcde16b08a745e1333c62c00dff73adf52
Speedwag00n <INemankov@luxoft.com>
2022-09-19 20:54:33 +0200

    Fix configdev.py downloading

--- vars/rpr_blenderplugin_pipeline.groovy
+++ vars/rpr_blenderplugin_pipeline.groovy
@@ -168,7 +168,7 @@ def executeTests(String osName, String asicName, Map options)
 
         String addonDir
         String prefsDir
-        String customKeys
+        String customKeys = ""
 
         withNotifications(title: options["stageName"], options: options, configuration: NotificationConfiguration.DOWNLOAD_PREFERENCES) {
             timeout(time: "5", unit: "MINUTES") {
@@ -178,6 +178,9 @@ def executeTests(String osName, String asicName, Map options)
                         addonDir = "/mnt/c/Users/${env.USERNAME}/AppData/Roaming/Blender Foundation/Blender/${options.toolVersion}/scripts/addons/rprblender"
                         break
                     case "MacOS_ARM":
+                        prefsDir = "/Users/${env.USER}/Library/Application Support/Blender/${options.toolVersion}/config"
+                        addonDir = "/Users/${env.USER}/Library/Application Support/Blender/${options.toolVersion}/scripts/addons/rprblender"
+                        break
                     case "OSX":
                         prefsDir = "/Users/${env.USER}/Library/Application Support/Blender/${options.toolVersion}/config"
                         addonDir = "/Users/${env.USER}/Library/Application Support/Blender/${options.toolVersion}/scripts/addons/rprblender"
@@ -202,7 +205,7 @@ def executeTests(String osName, String asicName, Map options)
                     println "[INFO] Install function on ${env.NODE_NAME} return ${newPluginInstalled}"
 
                     // Download configdev to enable collecting of debug information from RRP SDK
-                    downloadFiles("/volume1/CIS/configs/Blender/configdev.py", prefsDir, customKeys, false)
+                    downloadFiles("/volume1/CIS/configs/Blender/configdev.py", addonDir, customKeys, false)
                 }
             }
         
