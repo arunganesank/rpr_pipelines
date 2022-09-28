7e597fa61e7c34571bfef4517cb5d61a14f98fba
Speedwag00n <INemankov@luxoft.com>
2022-09-28 08:05:45 +0200

    Fix docker commands

--- vars/render_studio_deploy.groovy
+++ vars/render_studio_deploy.groovy
@@ -4,9 +4,9 @@ def call(
     node("RenderStudioServer") {
         dir("/usr/share/webusd/${options.deployEnvironment}") {
             sh """
-                docker-compose -f ${options.deployEnvironment} down
-                docker-compose -f ${options.deployEnvironment} pull
-                docker-compose -f ${options.deployEnvironment} up -d
+                docker-compose -f ${options.deployEnvironment}.yml down
+                docker-compose -f ${options.deployEnvironment}.yml pull
+                docker-compose -f ${options.deployEnvironment}.yml up -d
             """
         }
     } 
