c479547cf5dfe8ab308736625c5ecce7c007d1ad
Sofia Shikalova <sofshik@gmail.com>
2023-03-29 21:01:16 +0200

    Add missing comma

--- vars/summary.groovy
+++ vars/summary.groovy
@@ -3,10 +3,10 @@ import groovy.transform.Field
 import utils
 
 @Field final List jobUrls = [
-    "https://rpr.cis.luxoft.com/job/RenderStudio-Weekly",
-    "https://rpr.cis.luxoft.com/job/RPR-BlenderPlugin-Weekly",
-    "https://rpr.cis.luxoft.com/job/RPR-MayaPlugin-Weekly",
-    "https://rpr.cis.luxoft.com/job/RPR-BlenderPlugin-Auto"
+    "https://rpr.cis.luxoft.com/job/RenderStudio-Weekly/",
+    "https://rpr.cis.luxoft.com/job/RPR-BlenderPlugin-Weekly/",
+    "https://rpr.cis.luxoft.com/job/RPR-MayaPlugin-Weekly/",
+    "https://rpr.cis.luxoft.com/job/RPR-BlenderPlugin-Auto/"
     ]
 
 
@@ -31,7 +31,7 @@ def doRequest(String url) {
 def getColor(String result) {
     def colors = [
         "SUCCESS": "#5fbc34",
-        "UNSTABLE": '#b7950b',
+        "UNSTABLE": "#b7950b",
         "FAILURE": "b03a2e",
         "ABORTED": "14141f"
     ]
@@ -46,13 +46,13 @@ def call() {
                 ws("WS/Summary") {
                     currentBuild.description = ""
                     for (url in jobUrls) {
-                        def parsedJob = doRequest("${url}/api/json")
+                        def parsedJob = doRequest("${url}api/json")
                         if ("multibranch" in parsedJob["_class"]) {
                             def multiJobName = parsedJob["name"]
                             for (job in parsedJob["jobs"]) {
                                 def multiBuildName = job["name"]
                                 def buildUrl = job["url"]
-                                def parsedBuild = doRequest("${buildUrl}/api/json")
+                                def parsedBuild = doRequest("${buildUrl}api/json")
                                 def result = parsedBuild["result"]
                                 def color = getColor(result)
                                 currentBuild.description += "<span><a href='${buildUrl}'>${multiJobName} ${multiBuildName}</a> status: <span style='color: ${color}'>${result}</span>.</span><br/><br/>"
@@ -60,7 +60,7 @@ def call() {
                         }
                         else {
                             def jobName = parsedJob["name"]
-                            def parsedBuild = doRequest("${parsedJob["lastCompletedBuild"]["url"]}/api/json")
+                            def parsedBuild = doRequest("${parsedJob["lastCompletedBuild"]["url"]}api/json")
                             def result = parsedBuild["result"]
                             def buildUrl = parsedBuild["url"]
                             def color = getColor(result)
