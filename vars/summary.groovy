ab9aa2392177ceb3893becc565db75f80ecb27ac
Sofia Shikalova <sofshik@gmail.com>
2023-03-30 15:36:08 +0200

    Update summary.groovy

--- vars/summary.groovy
+++ vars/summary.groovy
@@ -3,10 +3,19 @@ import groovy.transform.Field
 import utils
 
 @Field final List jobUrls = [
-    "https://rpr.cis.luxoft.com/job/RenderStudio-Weekly/",
-    "https://rpr.cis.luxoft.com/job/RPR-BlenderPlugin-Weekly/",
-    "https://rpr.cis.luxoft.com/job/RPR-MayaPlugin-Weekly/",
-    "https://rpr.cis.luxoft.com/job/RPR-BlenderPlugin-Auto/"
+    "https://rpr.cis.luxoft.com/job/RPR-BlenderPlugin-Auto/",
+    "https://rpr.cis.luxoft.com/job/RPR-MayaPlugin-Auto/",
+    "https://rpr.cis.luxoft.com/job/USD-BlenderPlugin-Auto/",
+    "https://rpr.cis.luxoft.com/job/USD-MayaPlugin-Auto/",
+    "https://rpr.cis.luxoft.com/job/RenderStudio-Auto/",
+    "https://rpr.cis.luxoft.com/job/USD-HoudiniPlugin-Auto/",
+    "https://rpr.cis.luxoft.com/job/HdRPR-Auto/",
+    "https://rpr.cis.luxoft.com/job/HybridPro-Build-Auto/",
+    "https://rpr.cis.luxoft.com/job/HybridPro-Unit-Auto/",
+    "https://rpr.cis.luxoft.com/job/HybridPro-MTLX-Auto/",
+    "https://rpr.cis.luxoft.com/job/HybridPro-SDK-Auto/",
+    "https://rpr.cis.luxoft.com/job/RPR-SDK-Auto/",
+    "https://rpr.cis.luxoft.com/job/GithubReleases/"
     ]
 
 
@@ -48,19 +57,23 @@ def processUrl(String url) {
             def branchName = branch["name"]
             def parsedBranch = doRequest("${branch["url"]}api/json")
             def parsedBuild = doRequest("${parsedBranch["lastCompletedBuild"]["url"]}api/json")
-            def result = parsedBuild["result"]
             def buildUrl = parsedBuild["url"]
-            def color = getColor(result)
-            currentBuild.description += "<span><a href='${buildUrl}'>${multiJobName} ${branchName}</a> status: <span style='color: ${color}'>${result}</span>.</span><br/><br/>"
+            def result = parsedBuild["result"]
+            if (result == "FAILURE"){
+                def color = getColor(result)
+                currentBuild.description += "<span><a href='${buildUrl}'>${multiJobName} ${branchName}</a> status: <span style='color: ${color}'>${result}</span>.</span><br/><br/>"
+            }
         }
     }
     else {
         def jobName = parsedJob["name"]
         def parsedBuild = doRequest("${parsedJob["lastCompletedBuild"]["url"]}api/json")
-        def result = parsedBuild["result"]
         def buildUrl = parsedBuild["url"]
-        def color = getColor(result)
-        currentBuild.description += "<span><a href='${buildUrl}'>${jobName}</a> status: <span style='color: ${color}'>${result}</span>.</span><br/><br/>"
+        def result = parsedBuild["result"]
+        if (result == "FAILURE"){
+            def color = getColor(result)
+            currentBuild.description += "<span><a href='${buildUrl}'>${jobName}</a> status: <span style='color: ${color}'>${result}</span>.</span><br/><br/>"
+        }
     }
 }
 
@@ -73,6 +86,7 @@ def call() {
                     currentBuild.description = ""
                     for (url in jobUrls) {
                         processUrl(url)
+                        currentBuild.description += "<br/>"
                     }
                 }
             }
