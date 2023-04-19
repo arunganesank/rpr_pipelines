57a7290effb0226d606ebae6c087d27daf4ec5e0
Sofia Shikalova <sofshik@gmail.com>
2023-04-19 21:34:10 +0200

    Update weekly_results.groovy

--- vars/weekly_results.groovy
+++ vars/weekly_results.groovy
@@ -35,38 +35,40 @@ def generateInfo(){
     println("URLs: ${jobUrls}")
     for (jobUrl in jobUrls){
         def parsedJob = doRequest("${jobUrl}api/json")
-        def jobName = parsedJob["name"]
 
-        def parsedBuild = doRequest("${parsedJob["lastCompletedBuild"]["url"]}api/json")
-        def buildResult = parsedBuild["result"]
+        if (parsedJob["lastCompletedBuild"] != null){
+            def jobName = parsedJob["name"]
+            def parsedBuild = doRequest("${parsedJob["lastCompletedBuild"]["url"]}api/json")
+            def buildResult = parsedBuild["result"]
 
-        println("Job: ${jobName}. Result: ${buildResult}")
+            println("Job: ${jobName}. Result: ${buildResult}")
 
-        try{
-            def parsedSummary = doRequest("${parsedJob["lastCompletedBuild"]["url"]}artifact/summary_status.json")
-            problems = ["failed": parsedSummary["failed"], "error": parsedSummary["error"]]
+            try{
+                def parsedSummary = doRequest("${parsedJob["lastCompletedBuild"]["url"]}artifact/summary_status.json")
+                problems = ["failed": parsedSummary["failed"], "error": parsedSummary["error"]]
 
-            String problemsDescription = ""
+                String problemsDescription = ""
 
-            if (problems["failed"] > 0 && problems["error"] > 0) {
-                problemsDescription = "(${problems.failed} failed / ${problems.error} error)"
-            } else if (problems["failed"] > 0) {
-                problemsDescription = "(${problems.failed} failed)"
-            } else if (problems["error"] > 0) {
-                problemsDescription = "(${problems.error} error)"
+                if (problems["failed"] > 0 && problems["error"] > 0) {
+                    problemsDescription = "(${problems.failed} failed / ${problems.error} error)"
+                } else if (problems["failed"] > 0) {
+                    problemsDescription = "(${problems.failed} failed)"
+                } else if (problems["error"] > 0) {
+                    problemsDescription = "(${problems.error} error)"
+                }
+
+                if (buildResult == "FAILURE") {
+                    currentBuild.description += "<span style='color: #b03a2e; font-size: 150%'>${jobName} tests are Failed. ${problemsDescription}</span><br/><br/>"
+                } else if (buildResult == "UNSTABLE") {
+                    currentBuild.description += "<span style='color: #b7950b; font-size: 150%'>${jobName} tests are Unstable. ${problemsDescription}</span><br/><br/>"
+                } else if (buildResult == "SUCCESS") {
+                    currentBuild.description += "<span style='color: #5FBC34; font-size: 150%'>${jobName} tests are Success.</span><br/><br/>"
+                } else {
+                    currentBuild.description += "<span style='color: #b03a2e; font-size: 150%'>${jobName} tests with unexpected status. ${problemsDescription}</span><br/><br/>"
+                }
+            } catch (Exception e) {
+                currentBuild.description += "<span style='color: #b03a2e; font-size: 150%'>Failed to get ${jobName} summary_status.json.</span><br/><br/>"
             }
-
-            if (buildResult == "FAILURE") {
-                currentBuild.description += "<span style='color: #b03a2e; font-size: 150%'>${jobName} tests are Failed. ${problemsDescription}</span><br/><br/>"
-            } else if (buildResult == "UNSTABLE") {
-                currentBuild.description += "<span style='color: #b7950b; font-size: 150%'>${jobName} tests are Unstable. ${problemsDescription}</span><br/><br/>"
-            } else if (buildResult == "SUCCESS") {
-                currentBuild.description += "<span style='color: #5FBC34; font-size: 150%'>${jobName} tests are Success.</span><br/><br/>"
-            } else {
-                currentBuild.description += "<span style='color: #b03a2e; font-size: 150%'>${jobName} tests with unexpected status. ${problemsDescription}</span><br/><br/>"
-            }
-        } catch (Exception e) {
-            currentBuild.description += "<span style='color: #b03a2e; font-size: 150%'>Failed to get ${jobName} summary_status.json.</span><br/><br/>"
         }
     }
 }
