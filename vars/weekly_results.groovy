08491e4447478975c2f5183401d26938df61886c
Sofia Shikalova <sofshik@gmail.com>
2023-04-21 19:48:41 +0200

    Updates for debugging

--- vars/weekly_results.groovy
+++ vars/weekly_results.groovy
@@ -60,8 +60,8 @@ def getProblemsCount(String jobName, String buildUrl){
             def parsedReport = doRequest("${buildUrl}allure/data/suites.json")
             def failed = 0
 
-            println(parsedReport)
-            for (caseInfo in parsedReport){
+            for (caseInfo in parsedReport["children"]["children"]["children"]["children"]){
+                println(caseInfo)
                 if (caseInfo["status"] == "failed"){
                     failed += 1
                 }
