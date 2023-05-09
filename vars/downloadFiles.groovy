d8b01910bfd708b8d67597ff66ae191a6dc1bf93
Speedwag00n <INemankov@luxoft.com>
2023-05-09 17:52:42 +0200

    Draft impelemntation of rsync codes processing

--- vars/downloadFiles.groovy
+++ vars/downloadFiles.groovy
@@ -11,11 +11,13 @@ import org.jenkinsci.plugins.workflow.steps.FlowInterruptedException
 def call(String server_path, String local_path, String customKeys = "", Boolean clearEnv = true, String remoteHost = "nasURL", String sshPort = "nasSSHPort") {
     int times = 3
     int retries = 0
-    int status = 0
     String scriptFile = clearEnv ? "downloadFilesSync" : "downloadFiles"
+
     while (retries++ < times) {
         print("Try to download files with rsync №${retries}")
         try {
+            int status = 0
+
             withCredentials([string(credentialsId: remoteHost, variable: 'REMOTE_HOST'), string(credentialsId: sshPort, variable: "SSH_PORT")]) {
                 // Avoid warnings connected with using Groovy String interpolation with credentials
                 // See docs for more details: https://www.jenkins.io/doc/book/pipeline/jenkinsfile/#string-interpolation
@@ -27,10 +29,11 @@ def call(String server_path, String local_path, String customKeys = "", Boolean 
                         script: '%CIS_TOOLS%\\' + "${scriptFile}.bat \"${server_path}\" \"${local_path}\" " + '%REMOTE_HOST% %SSH_PORT%' + " \"${customKeys}\" ")
                 }
             }
-            if (status != 24) {
+
+            if (status == 0) {
                 return
             } else {
-                print("Partial transfer due to vanished source files")
+                print("[ERROR] Rsync returned non-zero exit code: ${status}")
             }
         } catch (FlowInterruptedException error) {
             println("[INFO] Job was aborted during downloading files from ${remoteHost}.")
@@ -39,7 +42,7 @@ def call(String server_path, String local_path, String customKeys = "", Boolean 
             println(e.toString())
             println(e.getMessage())
             println(e.getStackTrace())
+            sleep(60)
         }
-        sleep(60)
     }
 }
