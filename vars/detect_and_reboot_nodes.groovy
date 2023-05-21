383b8762f21a07553b6308fa07bf5be16b2060d4
NISHIY-EKSDEE <slava.astra@yahoo.com>
2023-05-21 14:14:52 +0200

    Surround csv files path with quotes. Print python scripts output

--- vars/detect_and_reboot_nodes.groovy
+++ vars/detect_and_reboot_nodes.groovy
@@ -4,10 +4,10 @@ def call(String EXECUTING_NODE) {
         withCredentials([usernamePassword(credentialsId: 'jenkinsCredentials', passwordVariable: 'JN_PASSWORD', usernameVariable: 'JN_USERNAME')]) {
             if (isUnix()) {
                 python3("-m pip install -r ${CIS_TOOLS}/autoreboot/requirements.txt")
-                python3("${CIS_TOOLS}/autoreboot/detect_and_reboot_nodes.py --pdus_file ${WORKSPACE}/pdus.csv --nodes_file ${WORKSPACE}/nodes_mapping.csv")
+                print(python3("${CIS_TOOLS}/autoreboot/detect_and_reboot_nodes.py --pdus_file \"${WORKSPACE}/pdus.csv\" --nodes_file \"${WORKSPACE}/nodes_mapping.csv\""))
             } else {
                 python3("-m pip install -r ${CIS_TOOLS}\\autoreboot\\requirements.txt")
-                python3("${CIS_TOOLS}\\autoreboot\\detect_and_reboot_nodes.py --pdus_file ${WORKSPACE}\\pdus.csv --nodes_file ${WORKSPACE}\\nodes_mapping.csv")
+                print(python3("${CIS_TOOLS}\\autoreboot\\detect_and_reboot_nodes.py --pdus_file \"${WORKSPACE}\\pdus.csv\" --nodes_file \"${WORKSPACE}\\nodes_mapping.csv\""))
             }
         }
     }
