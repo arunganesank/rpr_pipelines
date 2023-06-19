5f0d5ef2cb1d8445a769101cd620a6d58a36606b
Viacheslav Astrakhantsev <slava.astra@yahoo.com>
2023-06-19 10:37:11 +0200

    Pass jenkins url to env

--- vars/detect_and_reboot_nodes.groovy
+++ vars/detect_and_reboot_nodes.groovy
@@ -1,7 +1,7 @@
 def call(String EXECUTING_NODE) {
     node(EXECUTING_NODE) {
         downloadFiles("/volume1/CIS/PDUs/*.csv", "${WORKSPACE}", "", true)
-        withCredentials([usernamePassword(credentialsId: 'jenkinsCredentials', passwordVariable: 'JN_PASSWORD', usernameVariable: 'JN_USERNAME')]) {
+        withCredentials([string(credentialsId: 'jenkinsURL', variable: 'JN_URL'), usernamePassword(credentialsId: 'jenkinsCredentials', passwordVariable: 'JN_PASSWORD', usernameVariable: 'JN_USERNAME')]) {
             if (isUnix()) {
                 python3("-m pip install -r ${CIS_TOOLS}/autoreboot/requirements.txt")
                 print(python3("${CIS_TOOLS}/autoreboot/detect_and_reboot_nodes.py --pdus_file \"${WORKSPACE}/pdus.csv\" --nodes_file \"${WORKSPACE}/nodes_mapping.csv\""))
