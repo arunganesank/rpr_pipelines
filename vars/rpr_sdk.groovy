498092e528961b9a004303a1eb2687160d90ab49
Speedwag00n <INemankov@luxoft.com>
2023-06-20 20:32:19 +0200

    Fix RPR SDK pipeline

--- vars/rpr_sdk.groovy
+++ vars/rpr_sdk.groovy
@@ -10,8 +10,8 @@ import java.util.concurrent.atomic.AtomicInteger
 @Field final String PRODUCT_NAME = "AMD%20Radeon™%20ProRender%20Core"
 
 @Field final PipelineConfiguration PIPELINE_CONFIGURATION = new PipelineConfiguration(
-    supportedOS: ["Windows", "OSX", "Ubuntu18", "Ubuntu20"],
-    productExtensions: ["Windows": "zip", "OSX": "zip", "Ubuntu18": "zip", "Ubuntu20": "zip"],
+    supportedOS: ["Windows", "MacOS_ARM", "Ubuntu18", "Ubuntu20"],
+    productExtensions: ["Windows": "zip", "MacOS_ARM": "zip", "Ubuntu18": "zip", "Ubuntu20": "zip"],
     artifactNameBase: "binCore",
     testProfile: "engine",
     displayingProfilesMapping: [
@@ -317,9 +317,9 @@ def executeBuildOSX(Map options) {
 
             artifactURL = makeArchiveArtifacts(name: ARTIFACT_NAME, storeOnNAS: options.storeOnNAS)
 
-            makeStash(includes: ARTIFACT_NAME, name: getProduct.getStashName("OSX", options), preZip: false, storeOnNAS: options.storeOnNAS)
+            makeStash(includes: ARTIFACT_NAME, name: getProduct.getStashName("MacOS_ARM", options), preZip: false, storeOnNAS: options.storeOnNAS)
 
-            if (options.hipbinDownloadedOS == "OSX") {
+            if (options.hipbinDownloadedOS == "MacOS_ARM") {
                 dir("../../hipbin") {
                     // the Jenkins plugin can't perform git lfs pull on MacOS machines
                     sh """
@@ -379,6 +379,7 @@ def executeBuild(String osName, Map options)
                     executeBuildWindows(options)
                     break
                 case "OSX":
+                case "MacOS_ARM":
                     executeBuildOSX(options)
                     break
                 default:
@@ -786,8 +787,8 @@ def call(String projectBranch = "",
                 hipbinDownloadedOS = "Windows"
             } else if (platforms.contains("Ubuntu20")) {
                 hipbinDownloadedOS = "Ubuntu20"
-            } else if (platforms.contains("OSX")) {
-                hipbinDownloadedOS = "OSX"
+            } else if (platforms.contains("MacOS_ARM")) {
+                hipbinDownloadedOS = "MacOS_ARM"
             }
 
             options << [configuration: PIPELINE_CONFIGURATION,
