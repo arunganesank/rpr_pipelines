545a24a314da58a6d6dab04f8b8775a0d23bceb6
Sofia Shikalova <sofshik@gmail.com>
2023-06-21 13:19:17 +0200

    Increment_version.groovy

--- vars/increment_version.groovy
+++ vars/increment_version.groovy
@@ -4,54 +4,59 @@ import utils
 
 
 def incrementVersion() {
-    checkoutScm(branchName: "master", repositoryUrl: "git@github.com:GPUOpen-LibrariesAndSDKs/RadeonProRenderBlenderAddon.git")
-    def blenderVersion = version_read("${env.WORKSPACE}\\RadeonProRenderBlenderAddon\\src\\rprblender\\__init__.py", '"version": (', ', ')
-    println "[INFO] Current RPR Blender version: ${blenderVersion}"
+    dir('RadeonProRenderBlenderAddon') {
+        checkoutScm(branchName: "master", repositoryUrl: "git@github.com:GPUOpen-LibrariesAndSDKs/RadeonProRenderBlenderAddon.git")
+        def blenderVersion = version_read("${env.WORKSPACE}\\RadeonProRenderBlenderAddon\\src\\rprblender\\__init__.py", '"version": (', ', ')
+        println "[INFO] Current RPR Blender version: ${blenderVersion}"
 
-    def newMajorVersion = version_inc(version, 1, ', ')
-    println "[INFO] New major version: ${newMajorVersion}"
-    def newMinorVersion = version_inc(version, 2, ', ')
-    println "[INFO] New minor version: ${newMinorVersion}"
-    def newPatchVersion = version_inc(version, 3, ', ')
-    println "[INFO] New minor version: ${newPatchVersion}"
+        def newMajorVersion = version_inc(version, 1, ', ')
+        println "[INFO] New major version: ${newMajorVersion}"
+        def newMinorVersion = version_inc(version, 2, ', ')
+        println "[INFO] New minor version: ${newMinorVersion}"
+        def newPatchVersion = version_inc(version, 3, ', ')
+        println "[INFO] New minor version: ${newPatchVersion}"
 
-    version_write("${env.WORKSPACE}\\RadeonProRenderBlenderAddon\\src\\rprblender\\__init__.py", '"version": (', newPatchVersion, ', ')
+        version_write("${env.WORKSPACE}\\RadeonProRenderBlenderAddon\\src\\rprblender\\__init__.py", '"version": (', newPatchVersion, ', ')
 
-    blenderVersion = version_read("${env.WORKSPACE}\\RadeonProRenderBlenderAddon\\src\\rprblender\\__init__.py", '"version": (', ', ', "true").replace(', ', '.')
-    println "[INFO] Updated build version: ${blenderVersion}"
+        blenderVersion = version_read("${env.WORKSPACE}\\RadeonProRenderBlenderAddon\\src\\rprblender\\__init__.py", '"version": (', ', ', "true").replace(', ', '.')
+        println "[INFO] Updated build version: ${blenderVersion}"
+    }
 
-    checkoutScm(branchName: "master", repositoryUrl: "git@github.com:GPUOpen-LibrariesAndSDKs/RadeonProRenderMayaPlugin.git")
-    def mayaVersion = version_read("${env.WORKSPACE}\\RadeonProRenderMayaPlugin\\version.h", '#define PLUGIN_VERSION')
-    println "[INFO] Current RPR Maya version: ${mayaVersion}"
+    dir('RadeonProRenderMayaPlugin') {
+        checkoutScm(branchName: "master", repositoryUrl: "git@github.com:GPUOpen-LibrariesAndSDKs/RadeonProRenderMayaPlugin.git")
+        def mayaVersion = version_read("${env.WORKSPACE}\\RadeonProRenderMayaPlugin\\version.h", '#define PLUGIN_VERSION')
+        println "[INFO] Current RPR Maya version: ${mayaVersion}"
 
-    newMajorVersion = version_inc(version, 1)
-    println "[INFO] New major version: ${newMajorVersion}"
-    newMinorVersion = version_inc(version, 2)
-    println "[INFO] New minor version: ${newMinorVersion}"
-    newPatchVersion = version_inc(version, 3)
-    println "[INFO] New minor version: ${newPatchVersion}"
+        newMajorVersion = version_inc(version, 1)
+        println "[INFO] New major version: ${newMajorVersion}"
+        newMinorVersion = version_inc(version, 2)
+        println "[INFO] New minor version: ${newMinorVersion}"
+        newPatchVersion = version_inc(version, 3)
+        println "[INFO] New minor version: ${newPatchVersion}"
 
-    version_write("${env.WORKSPACE}\\RadeonProRenderMayaPlugin\\version.h", '#define PLUGIN_VERSION', newPatchVersion)
+        version_write("${env.WORKSPACE}\\RadeonProRenderMayaPlugin\\version.h", '#define PLUGIN_VERSION', newPatchVersion)
 
-    mayaVersion = version_read("${env.WORKSPACE}\\RadeonProRenderMayaPlugin\\version.h", '#define PLUGIN_VERSION')
-    println "[INFO] Updated build version: ${mayaVersion}"
+        mayaVersion = version_read("${env.WORKSPACE}\\RadeonProRenderMayaPlugin\\version.h", '#define PLUGIN_VERSION')
+        println "[INFO] Updated build version: ${mayaVersion}"
+    }
 
+    dir('AMDRenderStudio') {
+        checkoutScm(branchName: "main", repositoryUrl: "git@github.com:Radeon-Pro/RenderStudio.git")
+        def studioVersion = readFile("VERSION.txt").trim()
+        println "[INFO] Current Render Studio version: ${studioVersion}"
 
-    checkoutScm(branchName: "main", repositoryUrl: "git@github.com:Radeon-Pro/RenderStudio.git")
-    def studioVersion = readFile("VERSION.txt").trim()
-    println "[INFO] Current Render Studio version: ${studioVersion}"
+        newMajorVersion = version_inc(version, 1)
+        println "[INFO] New major version: ${newMajorVersion}"
+        newMinorVersion = version_inc(version, 2)
+        println "[INFO] New minor version: ${newMinorVersion}"
+        newPatchVersion = version_inc(version, 3)
+        println "[INFO] New minor version: ${newPatchVersion}"
 
-    newMajorVersion = version_inc(version, 1)
-    println "[INFO] New major version: ${newMajorVersion}"
-    newMinorVersion = version_inc(version, 2)
-    println "[INFO] New minor version: ${newMinorVersion}"
-    newPatchVersion = version_inc(version, 3)
-    println "[INFO] New minor version: ${newPatchVersion}"
+        writeFile(file: "VERSION.txt", text: newPatchVersion)
 
-    writeFile(file: "VERSION.txt", text: newPatchVersion)
-
-    studioVersion = readFile("VERSION.txt").trim()
-    println "[INFO] Updated build version: ${studioVersion}"
+        studioVersion = readFile("VERSION.txt").trim()
+        println "[INFO] Updated build version: ${studioVersion}"
+    }
 }
 
 
