6f5596ea0f6d24d1621239021df6ee80891d8211
Sofia Shikalova <sofshik@gmail.com>
2023-06-29 20:29:04 +0200

    Clean_temp.groovy initial

---
+++ vars/clean_temp.groovy
@@ -0,0 +1,49 @@
+import groovy.transform.Field
+import org.apache.commons.io.FileUtils
+import jenkins.model.*
+
+
+@NonCPS
+def getNodes(String label) {
+    jenkins.model.Jenkins.instance.nodes.collect { thisAgent ->
+        if (thisAgent.labelString.contains("${label}")) {
+            return thisAgent.name
+        }
+    }
+}
+
+
+def cleanTemp(String agentName) {
+    node("${agentName}") {
+        stage("Cleaning Temp on ${agentName}") {
+            File temp = new File("%APPDATA%\\Local\\Temp")
+            FileUtils.cleanDirectory(temp)
+        }
+    }
+}
+
+
+def clean() {
+    def nodeList = getNodes("Windows")
+    
+    for(i = 0; i < nodeList.size(); i++) {
+        def agentName = nodeList[i]
+        println(agentName)
+
+        //if (agentName != null) {
+            //println "Cleaning %TEMP% on " + agentName
+            //cleanTemp(agentName)
+        //}
+    }
+}
+
+
+def call(){
+    timestamps {
+        stage("Clean Temp directory") {
+            node("Windows && PreBuild") {
+                clean()
+            }
+        }
+    }
+}
