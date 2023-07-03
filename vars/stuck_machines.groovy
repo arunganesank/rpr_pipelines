c1dbdb4cc3b6c1acc4c6db63ba1ac1255365740b
Pavel Sergeev <pavel.al.sergeev@gmail.com>
2023-07-03 17:42:37 +0200

    add script to find stuck machines

---
+++ vars/stuck_machines.groovy
@@ -0,0 +1,41 @@
+static def checkNodes() {
+    // Get all the nodes
+    def nodes = Jenkins.instance.nodes
+
+    nodes.each { node ->
+        checkComputer(node)
+    }
+}
+
+
+def checkComputer(node) {
+    def computer = node.getComputer()
+    if (computer.online) {
+        def stuckExecutors = 0
+
+        if (computer.countBusy() > 0) {
+            def executors = computer.executors
+            executors.each { executor ->
+                if (executor.isLikelyStuck()) {
+                    echo "Executor ${executor.displayName} on node ${node.displayName} is likely stuck"
+                    stuckExecutors += 1
+                }
+            }
+        }
+
+        // Check if all executors on the node are stuck
+        if (node.getNumExecutors() == stuckExecutors) {
+            echo "Node ${node.displayName} is likely stuck"
+            SlackUtils.sendMessageToWorkspaceChannel(self, '', oldDriverMessage, SlackUtils.Color.ORANGE, SlackUtils.SlackWorkspace.LUXCIS, 'cis_notifications')
+        }
+    }
+}
+
+def call() {
+    // main function
+    timestamps {
+        stage("Check if node is stuck") {
+            checkNodes()
+        }
+    }
+}
