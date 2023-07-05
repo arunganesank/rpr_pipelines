5ad3ddd73ddcbff64f611bf2d0bc1741f90a3534
Sofia Shikalova <sofshik@gmail.com>
2023-07-05 18:15:11 +0200

    Update_sceneLM.groovy initial

---
+++ vars/update_sceneLM.groovy
@@ -0,0 +1,29 @@
+import groovy.transform.Field
+
+import utils
+
+
+def getLatestName() {
+    withCredentials([string(credentialsId: "nasURL", variable: "REMOTE_HOST"), string(credentialsId: "nasSSHPort", variable: "SSH_PORT")]) {
+        def fileNames = bat(returnStdout: true, script: '%CIS_TOOLS%\\' 
+            + "listFiles.bat \"/volume1/Shared/Art NAS files/USDScenes\" " + '%REMOTE_HOST% %SSH_PORT% -t').split("\n") as List
+        return fileNames[0]
+    }
+}
+
+
+def call() {
+    timestamps {
+        stage("Update LiveMode scene") {
+            node("Windows && PreBuild") {
+                def fileName = getLatestName()
+                downloadFiles("/volume1/Shared/Art NAS files/USDScenes/${fileName}", ".")
+
+                unzip(zipFile: fileName)
+
+                def folderName = fileName.take(fileName.lastIndexOf("."))
+                uploadFiles("./${folderName}", "/volume1/web/Assets/render_studio_autotests/Scene_002_usd")
+            }
+        }
+    }
+}
