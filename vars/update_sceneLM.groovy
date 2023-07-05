1a7e2ddf002d8b336a10c33b9e147e923e29be6e
Sofia Shikalova <sofshik@gmail.com>
2023-07-05 19:37:12 +0200

    Escape spaces

--- vars/update_sceneLM.groovy
+++ vars/update_sceneLM.groovy
@@ -6,7 +6,7 @@ import utils
 def getLatestName() {
     withCredentials([string(credentialsId: "nasURL", variable: "REMOTE_HOST"), string(credentialsId: "nasSSHPort", variable: "SSH_PORT")]) {
         def fileNames = bat(returnStdout: true, script: '%CIS_TOOLS%\\' 
-            + "listFiles.bat \"/volume1/Shared/Art NAS files/USDScenes\" " + '%REMOTE_HOST% %SSH_PORT% -t').split("\n") as List
+            + "listFiles.bat \"/volume1/Shared/Art\ NAS\ files/USDScenes\" " + '%REMOTE_HOST% %SSH_PORT% -t').split("\n") as List
             println(fileNames)
         return fileNames[0]
     }
@@ -19,7 +19,7 @@ def call() {
             node("Windows && PreBuild") {
                 def fileName = getLatestName()
                 println(fileName)
-                downloadFiles("/volume1/Shared/Art NAS files/USDScenes/${fileName}", ".")
+                downloadFiles("/volume1/Shared/Art\ NAS\ files/USDScenes/${fileName}", ".")
 
                 unzip(zipFile: fileName)
 
