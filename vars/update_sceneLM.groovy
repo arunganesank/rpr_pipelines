897096578727d7e72f044b0015f028f01990a835
Sofia Shikalova <sofshik@gmail.com>
2023-07-05 18:59:47 +0200

    Add changes for debugging

--- vars/update_sceneLM.groovy
+++ vars/update_sceneLM.groovy
@@ -7,6 +7,7 @@ def getLatestName() {
     withCredentials([string(credentialsId: "nasURL", variable: "REMOTE_HOST"), string(credentialsId: "nasSSHPort", variable: "SSH_PORT")]) {
         def fileNames = bat(returnStdout: true, script: '%CIS_TOOLS%\\' 
             + "listFiles.bat \"/volume1/Shared/Art NAS files/USDScenes\" " + '%REMOTE_HOST% %SSH_PORT% -t').split("\n") as List
+            println(fileNames)
         return fileNames[0]
     }
 }
@@ -17,11 +18,13 @@ def call() {
         stage("Update LiveMode scene") {
             node("Windows && PreBuild") {
                 def fileName = getLatestName()
+                println(fileName)
                 downloadFiles("/volume1/Shared/Art NAS files/USDScenes/${fileName}", ".")
 
                 unzip(zipFile: fileName)
 
                 def folderName = fileName.take(fileName.lastIndexOf("."))
+                println(folderName)
                 uploadFiles("./${folderName}", "/volume1/web/Assets/render_studio_autotests/Scene_002_usd")
             }
         }
