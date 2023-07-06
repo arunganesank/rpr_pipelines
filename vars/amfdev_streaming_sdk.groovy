0f1d36dc53d551d335163b100adeb570f47bc31e
Pavel Sergeev <pavel.al.sergeev@gmail.com>
2023-07-06 18:26:43 +0200

    use amdDriverUpdate once more to check

--- vars/amfdev_streaming_sdk.groovy
+++ vars/amfdev_streaming_sdk.groovy
@@ -1126,7 +1126,8 @@ def executeTests(String osName, String asicName, Map options) {
 
     try {
 
-        utils.updateDriver(options, osName, "")
+        // utils.updateDriver(options, osName, "")
+        amdDriverUpdate(productionDriver = False, platforms = options.platforms)
 
         if (osName == "Windows" || osName == "Ubuntu20") {
             options["clientInfo"] = new ConcurrentHashMap()
