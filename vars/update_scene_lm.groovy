5ba9885c1e88199438d28360729ce103df271a5b
Speedwag00n <INemankov@luxoft.com>
2023-07-07 09:41:00 +0200

    Code-style updates

--- vars/update_scene_lm.groovy
+++ vars/update_scene_lm.groovy
@@ -1,13 +1,12 @@
 import groovy.transform.Field
-
 import utils
 
 
-def getLatestName() {
+def getLatestArchiveName() {
     withCredentials([string(credentialsId: "nasURL", variable: "REMOTE_HOST"), string(credentialsId: "nasSSHPort", variable: "SSH_PORT")]) {
         def fileNames = bat(returnStdout: true, script: '%CIS_TOOLS%\\' 
             + "listFiles.bat \"/volume1/Shared/Art_NAS_files/USDScenes\" " + '%REMOTE_HOST% %SSH_PORT% -t').split("\n") as List
-            println(fileNames)
+        println("Found archives: ${fileNames}")
         return fileNames[0]
     }
 }
@@ -17,14 +16,14 @@ def call() {
     timestamps {
         stage("Update LiveMode scene") {
             node("Windows && PreBuild") {
-                def fileName = getLatestName()
-                println(fileName)
+                def fileName = getLatestArchiveName()
+                println("Latest archive name: ${fileName}")
                 downloadFiles("/volume1/Shared/Art_NAS_files/USDScenes/${fileName}", ".")
 
                 unzip(zipFile: fileName)
 
                 def folderName = fileName.take(fileName.lastIndexOf("."))
-                println(folderName)
+                println("Root folder name: ${folderName}")
                 uploadFiles("./${folderName}/usd", "/volume1/web/Assets/render_studio_autotests/Scene_002_usd")
                 uploadFiles("./${folderName}/Scene002.blend", "/volume1/web/Assets/render_studio_autotests/Scene_002_usd")
             }
