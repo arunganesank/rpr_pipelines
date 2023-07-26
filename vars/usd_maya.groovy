c407a3766c787378d13bc6b284a0f99e83022b14
Speedwag00n <INemankov@luxoft.com>
2023-07-26 23:33:26 +0200

    Fix RS_Maya_LiveMode timeout parsing

--- vars/usd_maya.groovy
+++ vars/usd_maya.groovy
@@ -888,7 +888,7 @@ def executePreBuild(Map options) {
             } else if (options.tests) {
                 options.tests = utils.uniteSuites(this, "jobs/weights.json", options.tests.split(" ") as List)
                 options.tests.each() {
-                    def xml_timeout = utils.getTimeoutFromXML(this, it, "simpleRender.py", options.ADDITIONAL_XML_TIMEOUT)
+                    def xml_timeout = utils.getTimeoutFromXML(this, it, "simple", options.ADDITIONAL_XML_TIMEOUT)
                     options.timeouts["${it}"] = (xml_timeout > 0) ? xml_timeout : options.TEST_TIMEOUT
                 }
                 options.engines.each { engine ->
